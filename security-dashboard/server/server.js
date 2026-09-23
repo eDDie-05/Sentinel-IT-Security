3b2cb87397f89a06b7cf358d05ee037fd8b03813
George Mollel <georgemollel@Georges-iMac.local>
2026-09-23 14:16:16 +0300

    render4

--- security-dashboard/server/server.js
+++ security-dashboard/server/server.js
@@ -6235,6 +6235,13 @@ app.use(
 /* =========================================================
    GLOBAL ERROR HANDLER
 ========================================================= */
+const frontendPath = path.join(process.cwd(), "dist");
+
+app.use(express.static(frontendPath));
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(frontendPath, "index.html"));
+});
 
 app.use(
   (
